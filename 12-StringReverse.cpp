b5051a4d72aaaf35bfb9ad84461d04a8f6b4e6f3
KavyaJP <kavya31052005@gmail.com>
2024-06-27 14:45:52 +0530

    Question 12

---
+++ 12-StringReverse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c[50];
+    int len=0;
+    cout<<"Enter a String : ";
+    gets(c);
+    for (int i = 0; c[i]!='\0'; i++)
+    {
+        len++;
+    }
+    cout<<"Reverse of the String is : ";
+    for (int i = len; i >= 0; i--)
+    {
+        cout<<c[i];
+    }
+    return 0;
+}
