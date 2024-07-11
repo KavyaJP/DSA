1aab424bd200315bb6408f9967c18efaf4757d73
KavyaJP <kavya31052005@gmail.com>
2024-07-11 15:39:11 +0530

    Practical 2.1

---
+++ 23AIML056_Prac2_1_A.cpp
@@ -0,0 +1,31 @@
+// Implement Bubble Sort Algorithm
+#include <iostream>
+using namespace std;
+void BubbleSort(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    BubbleSort(arr, n);
+}
