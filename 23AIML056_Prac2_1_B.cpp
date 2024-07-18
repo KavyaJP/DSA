f592d918f1256630b8b43a8a96bc1f3c03e26aba
KavyaJP <kavya31052005@gmail.com>
2024-07-18 15:20:42 +0530

    Number of Swaps and Passes

--- 23AIML056_Prac2_1_B.cpp
+++ 23AIML056_Prac2_1_B.cpp
@@ -3,14 +3,26 @@
 using namespace std;
 void SelectionSort(int a[], int n)
 {
+    int swap_count = 0, pass_count = 0;
     for (int i = 0; i < n - 1; i++)
     {
         int min_idx = i;
         for (int j = i + 1; j < n; j++)
+        {
             if (a[j] < a[min_idx])
+            {
+                pass_count++;
                 min_idx = j;
-        swap(a[min_idx], a[i]);
+            }
+        }
+        if (a[min_idx] != a[i])
+        {
+            swap(a[min_idx], a[i]);
+            swap_count++;
+        }
     }
+    cout << "Number of passes: " << pass_count << endl
+         << "Number of swaps: " << swap_count << endl;
 }
 int main()
 {
